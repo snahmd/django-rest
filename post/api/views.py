150a2fc081d917f4d4120787f16d3b6efb6be2b4
snahmd <sanahmed7@gmail.com>
2020-02-27 00:38:51 +0100

    json with serializer

--- post/api/views.py
+++ post/api/views.py
@@ -1,7 +1,9 @@
 from rest_framework.generics import ListAPIView
+
 from post.models import Post
+from post.api.serializers import PostSerializer
 
 class PostListAPIView(ListAPIView):
   queryset = Post.objects.all()
-
+  serializer_class = PostSerializer
   
